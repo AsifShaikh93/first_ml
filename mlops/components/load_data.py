6ddec363277fd7d5a14b353b619c6cbdfe8e989e
AsifShaikh93 <158223417+AsifShaikh93@users.noreply.github.com>
2025-12-14 19:17:02 +0530

    Update load_data.py

--- mlops/components/load_data.py
+++ mlops/components/load_data.py
@@ -1,1 +1,18 @@
+# load_data.py
+import pandas as pd
+
+DATA_URL = "https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/diabetes.csv"
+
+FEATURE_COLUMNS = ["Pregnancies", "Glucose", "BloodPressure", "BMI", "Age"]
+TARGET_COLUMN = "Outcome"
+
+
+def load_data():
+    df = pd.read_csv(DATA_URL)
+    print("Columns:", df.columns.tolist())
+
+    X = df[FEATURE_COLUMNS]
+    y = df[TARGET_COLUMN]
+
+    return X, y
 
