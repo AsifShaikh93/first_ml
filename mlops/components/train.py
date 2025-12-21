3bc8f8c30b3310b5153773746322288549b36480
AsifShaikh93 <158223417+AsifShaikh93@users.noreply.github.com>
2025-12-22 03:34:33 +0530

    Update train.py

--- mlops/components/train.py
+++ mlops/components/train.py
@@ -15,7 +15,7 @@ def train_model(X, y):
         mlflow.log_param("random_state", 42)
 
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42
+            X, y, test_size=0.2, random_state=42, stratify=y
         )
 
         model = RandomForestClassifier(n_estimators=200,max_depth=10,random_state=42)
