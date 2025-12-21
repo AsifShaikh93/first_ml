20c24876d1e98cb74f0f065e9cd70e7cbf0c83bf
AsifShaikh93 <158223417+AsifShaikh93@users.noreply.github.com>
2025-12-22 03:07:16 +0530

    Update save_model.py

--- mlops/components/save_model.py
+++ mlops/components/save_model.py
@@ -5,26 +5,13 @@ import os
 
 
 def save_model(model) -> str:
-    """
-    Logs and registers a model in MLflow Model Registry.
-
-    Returns:
-        model_uri (str): MLflow model URI (used for comparison/registration)
-    """
-
-    # ---- CONFIG (adjust only if needed) ----
     MODEL_NAME = "diabetes-model"
     EXPERIMENT_NAME = "diabetes-ct"
 
-    # Optional: set explicitly if not injected via env
     mlflow.set_tracking_uri('https://mlflow-apirule.c-321a6c0.stage.kyma.ondemand.com')
-
-    # ---------------------------------------
-
     mlflow.set_experiment(EXPERIMENT_NAME)
 
-    with mlflow.start_run() as train:
-        # Log the model
+    with mlflow.start_run() as run: 
         mlflow.sklearn.log_model(
             sk_model=model,
             artifact_path="model",
@@ -32,5 +19,6 @@ def save_model(model) -> str:
         )
 
         model_uri = f"runs:/{run.info.run_id}/model"
+        print(f"Model registered at: {model_uri}")
 
     return model_uri
