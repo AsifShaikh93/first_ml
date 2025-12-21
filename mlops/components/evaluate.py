aff47eaceadaa17c846067815a0978162aab2e71
AsifShaikh93 <158223417+AsifShaikh93@users.noreply.github.com>
2025-12-22 03:29:01 +0530

    Update evaluate.py

--- mlops/components/evaluate.py
+++ mlops/components/evaluate.py
@@ -5,6 +5,7 @@ from sklearn.metrics import accuracy_score
 
 def evaluate_model(model, X_test, y_test):
     accuracy = accuracy_score(y_test, model.predict(X_test))
+    mlflow.set_tracking_uri("https://mlflow-apirule.c-321a6c0.stage.kyma.ondemand.com")
 
     mlflow.log_metric("accuracy", accuracy)
 
