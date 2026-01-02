dbc5dee522a44969a58a0ae98c6282a960e76ecf
AsifShaikh93 <158223417+AsifShaikh93@users.noreply.github.com>
2026-01-02 22:54:17 +0000

    hbskjfwejn

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+# main.py
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -7,6 +9,10 @@ import numpy as np
 import joblib
 import os
 import tempfile
+import mlflow
+from mlflow.tracking import MlflowClient
+import threading
+import time
 
 app = FastAPI()
 
@@ -16,11 +22,14 @@ templates = Jinja2Templates(directory="app/templates")
 
 # ENV: Model Registry URI
 MODEL_URI = os.getenv("MODEL_URI")
-
-if not MODEL_URI:
-    raise RuntimeError("MODEL_URI environment variable is not set")
+MODEL_NAME = "diabetes-model"
+ALIAS = "Production"
 
 model = None
+current_version = None
+lock = threading.Lock()
+
+
 
 
 class DiabetesInput(BaseModel):
@@ -31,32 +40,23 @@ class DiabetesInput(BaseModel):
     Age: int
 
 
-def download_model(model_uri: str) -> str:
-    """
-    Download model artifact from Model Registry (HTTP / S3 / MinIO)
-    Returns local file path
-    """
-    print(f"[model] Downloading model from {model_uri}")
-
-    response = requests.get(model_uri)
-    response.raise_for_status()
-
-    tmp = tempfile.NamedTemporaryFile(delete=False, suffix=".pkl")
-    tmp.write(response.content)
-    tmp.close()
-
-    print(f"[model] Model downloaded to {tmp.name}")
-    return tmp.name
 
 
-@app.on_event("startup")
-def load_model():
-    global model
 
-    model_path = download_model(MODEL_URI)
-    model = joblib.load(model_path)
+def load_model_if_changed():
+    global model, current_version
 
-    print("[model] Model loaded successfully")
+    client = MlflowClient()
+    mv = client.get_model_version_by_alias(MODEL_NAME, ALIAS)
+
+    if current_version != mv.version:
+        print(f"[model] Loading new model version: v{mv.version}")
+        new_model = mlflow.pyfunc.load_model(f"models:/{MODEL_NAME}/{ALIAS}")
+
+        with lock:
+            model = new_model
+            current_version = mv.version
+            print("[model] Model swapped successfully")
 
 
 @app.get("/", response_class=HTMLResponse)
