797a947080a5dde8645ed7ba1a4f9ebe29c6130b
AsifShaikh93 <158223417+AsifShaikh93@users.noreply.github.com>
2026-01-02 13:14:56 +0000

    jvWJHWDdggergewredgergr

--- mlops/steps/train.py
+++ mlops/steps/train.py
@@ -3,6 +3,13 @@ import joblib
 from mlops.components.train import train_model
 import os
 
+class SklearnWrapper(mlflow.pyfunc.PythonModel):
+    def load_context(self, context):
+        self.model = joblib.load(context.artifacts["model"])
+
+    def predict(self, context, model_input):
+        return self.model.predict(model_input)
+
 def train():
      
     with mlflow.start_run(run_name="train") as run:
@@ -11,10 +18,14 @@ def train():
         y=joblib.load("y.pkl")
         model, X_test, y_test = train_model(X,y)
 
-        os.makedirs("model", exist_ok=True)
-        joblib.dump(model, "model/model.pkl")
+        os.makedirs("artifacts", exist_ok=True)
+        joblib.dump(model, "artifacts/model.pkl")
 
-        mlflow.log_artifacts("model", artifact_path="model")
+        mlflow.pyfunc.log_model(
+            artifact_path="model",
+            python_model=SklearnWrapper(),
+            artifacts={"model": "artifacts/model.pkl"}
+        )
         
         print(f"TRAIN_RUN_ID={run.info.run_id}")
 
