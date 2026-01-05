1fdb80a4f29ea0fa819c0e41fa3005be69a83462
AsifShaikh93 <158223417+AsifShaikh93@users.noreply.github.com>
2026-01-05 20:40:50 +0000

    jvsfherehrt

--- mlops/steps/ingest.py
+++ mlops/steps/ingest.py
@@ -6,15 +6,15 @@ import argparse
 def ingest():
     X, y, DATA_URL, FEATURE_COLUMNS, TARGET_COLUMN = load_data()
 
-    with mlflow.start_run(run_name="ingest") as run:
-        mlflow.log_param("rows", len(X))
-        mlflow.log_param("DATA_URL", DATA_URL)
-        mlflow.log_param("FEATURE_COLUMNS", FEATURE_COLUMNS)
-        mlflow.log_param("TARGET_COLUMN", TARGET_COLUMN)
-        joblib.dump(X, "X.pkl")
-        joblib.dump(y, "y.pkl")
+    mlflow.start_run(run_name="ingest") 
+    mlflow.log_param("rows", len(X))
+    mlflow.log_param("DATA_URL", DATA_URL)
+    mlflow.log_param("FEATURE_COLUMNS", FEATURE_COLUMNS)
+    mlflow.log_param("TARGET_COLUMN", TARGET_COLUMN)
+    joblib.dump(X, "X.pkl")
+    joblib.dump(y, "y.pkl")
 
-        print(f"TRAIN_RUN_ID={run.info.run_id}")
+    print(f"TRAIN_RUN_ID={mlflow.active_run().info.run_id}")
 
 if __name__ == "__main__":
     ingest()        
